b6ca32cd17c501509cabc4515b2b00eacca85bd2
Adam Post <adamrpostjr@gmail.com>
2023-10-11 21:26:54 -0400

    init project

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+	import '../app.postcss';
+	import { initializeStores } from '@skeletonlabs/skeleton';
+
+	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
+	import { storePopup, Toast, Modal } from '@skeletonlabs/skeleton';
+
+	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+	initializeStores();
+</script>
+
+<Toast />
+<Modal />
+
+<slot />
