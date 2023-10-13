1dd3fd7482b5dcd7f6b6034534b828537ea6c2d8
Adam Post <adamrpostjr@gmail.com>
2023-10-12 21:40:08 -0400

    Started Mocking up the login page

---
+++ src/routes/(auth)/auth/+page.server.js
@@ -0,0 +1,14 @@
+export const actions = {
+	login: async (event) => {
+		// TODO log the user in
+	},
+    forgotPassword: async (event) => {
+        // TODO send the user a password reset email
+    },
+    regTwoFA: async (event) => {
+        // TODO register the user's 2FA
+    },
+    verifyTwoFA: async (event) => {
+        // TODO verify the user's 2FA
+    }
+};
---
+++ src/routes/(auth)/auth/+page.svelte
@@ -0,0 +1,34 @@
+<script>
+
+</script>
+
+
+<div class="variant-glass-surface absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 h-1/2 w-2/3 lg:relative lg:left-0 lg:top-0 lg:translate-x-0 lg:translate-y-0 lg:h-full lg:w-1/4 shadow">
+    <div class="flex flex-col gap-5 h-full">
+        <div class="mx-auto h-24 flex flex-col justify-center"> <!-- Add Logo here instead of div -->
+            LOGO HERE
+        </div>
+        <form class="grid gap-5 px-5 my-auto grow h-fit">
+            <div class="input-group input-group-divider grid-cols-[auto_1fr_auto]">
+                <div class="input-group-shim">User</div>
+                <input type="text" placeholder="admin@openrmm.com" />
+              </div>
+              <div class="input-group input-group-divider grid-cols-[auto_1fr_auto]">
+                <div class="input-group-shim">Pass</div>
+                <input type="text" placeholder="***************" />
+              </div>
+              <div class="grid gap-2">
+                  <button class="btn variant-filled-primary">Login</button>
+                  <button class="w-fit ml-auto text-sm">Forgot Password</button>
+              </div>
+        </form>
+        <div>
+            <a target="_blank" href="https://github.com/adamrpostjr/OpenRMM" class="flex gap-5 justify-center">
+                <iconify-icon icon="mdi:github" width="32"></iconify-icon>
+                <span class="flex flex-col justify-center">
+                    Github
+                </span>
+            </a>
+        </div>
+    </div>
+</div>
